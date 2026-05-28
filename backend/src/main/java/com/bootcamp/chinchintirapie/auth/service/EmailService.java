4542a88ffcb48a578b6cdcf08794d5ea4be67099
Brandon Inostroza Canio <brandondic@outlook.cl>
2026-05-28 02:32:53 -0400

    fix: se agregar cambios para la funcionalidad de recuperar contraseña

--- backend/src/main/java/com/bootcamp/chinchintirapie/auth/service/EmailService.java
+++ backend/src/main/java/com/bootcamp/chinchintirapie/auth/service/EmailService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,7 @@ public class EmailService {
     @Value("${spring.mail.properties.mail.from:noreply@chinchintirapie.com}")
     private String fromEmail;
 
+    @Async
     public void sendPasswordResetEmail(String to, String resetLink) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(fromEmail);
